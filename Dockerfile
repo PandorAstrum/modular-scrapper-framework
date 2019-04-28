6562fdd92e7a23923e67189a56aedfe676a9472e
PandorAstrum <primeintegerslab@gmail.com>
2019-04-28 12:45:40 +0600

    proper Docker, API Endpoints updates

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,29 @@
-FROM scrapinghub/scrapinghub-stack-scrapy:1.3
-ENV TERM xterm
-ENV SCRAPY_SETTINGS_MODULE general.settings
-RUN mkdir -p /app
+# Use an official Python runtime as a parent image
+FROM python:3.6
+
+# Set the working directory to /app
 WORKDIR /app
-COPY ./requirements.txt /app/requirements.txt
-RUN pip install --no-cache-dir -r requirements.txt
-COPY . /app
-RUN python setup.py install
+
+# Copy the current directory contents into the container at /app
+ADD . /app
+
+# Install Twisted wheel Before To prevent C++ build tools look up
+RUN pip install Twisted-19.2.0-cp36-cp36m-win_amd64.whl
+
+# Install any needed packages specified in requirements.txt
+RUN pip install -r requirements.txt
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# Define environment variable
+ENV NAME World
+
+# Run scrapy server mutithreaded job system
+CMD ["scrapyd"]
+
+# Create egg of spiders and deploy to Server
+RUN scrapyd-deploy local
+
+# Run app.py when the container launches
+CMD ["python", "api.py"]
--- api.py
+++ api.py
@@ -1,47 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 __author__ = "Ashiquzzaman Khan"
-__desc__ = "Description of this file here"
+__desc__ = "API To communicate with the flask app"
 """
 from __future__ import unicode_literals
-
-import os
+# import os
 from subprocess import Popen, PIPE
-import json
+# import json
 import requests
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, abort
 import utility
-from general.run_scrapper import scrape_product, scrape_price
-import uuid
-from werkzeug.security import generate_password_hash, check_password_hash
-# import jwt
-import datetime
-from functools import wraps
-
+#
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisissecret'
-
-# def token_required(f):
-#     @wraps(f)
-#     def decorated(*args, **kwargs):
-#         token = None
-#
-#         if 'x-access-token' in request.headers:
-#             token = request.headers['x-access-token']
-#
-#         if not token:
-#             return jsonify({'message' : 'Token is missing!'}), 401
-#
-#         try:
-#             data = jwt.decode(token, app.config['SECRET_KEY'])
-#             current_user = User.query.filter_by(public_id=data['public_id']).first()
-#         except:
-#             return jsonify({'message' : 'Token is invalid!'}), 401
-#
-#         return f(current_user, *args, **kwargs)
-#
-#     return decorated
 
 
 def get_scrapper(with_index=False):
@@ -62,39 +34,62 @@ def get_scrapper(with_index=False):
 @app.route('/', methods=['GET'])
 def home():
     # get all the spider in a list and display
-    _tmp_spider_list = get_scrapper(True)
-    return jsonify(_tmp_spider_list)
+
+    return jsonify("")
+
+
+@app.route('/jobs', methods=['GET'])
+def jobs():
+    # execute command scrapyd
+    _p = Popen(['curl', '-X', 'GET', 'http://127.0.0.1:6800/listjobs.json?project=general'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    _output, _err = _p.communicate(b"stdin")
+    rc = _p.returncode
+
+    return _output
+
+
+@app.route('/jobs-verbose', methods=['GET'])
+def jobs_verbose():
+    # get Detailed job list
+    _p = Popen(['curl', '-X', 'GET', 'http://127.0.0.1:6800/jobs'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    _output, _err = _p.communicate(b"stdin")
+    rc = _p.returncode
+    return _output
 
 
 @app.route('/spider/<spider_name>', methods=['GET'])
 def run_products(spider_name):
+    # schedule a spider to run
     _spider_name = spider_name
+
     _tmp_spider_list = get_scrapper()
-
     if _spider_name not in _tmp_spider_list:
-        return jsonify({"Error": f"No Spider Named {_spider_name}"})
+        abort(404)
     else:
-        params = {
-            "_username": None,
-            "_password": None,
-            "_signin": False,
-            "_spider": _spider_name,
-            "spider_name": _spider_name,
-            "start_requests": True
-        }
-        # async run or pool here
-        response = requests.get('http://localhost:9080/crawl.json', params)
-        _data = json.loads(response.text)
+        # create output filename with path
+        _path= "C:\\Users\\Ana Ash\\Desktop\\skrapy3\\project"
+        _filename= utility.get_output_file(_file_name=f"\\{_spider_name}_Products")
+        # $ curl
+        # http://localhost:6800/schedule.json -d project= <projectname> -d spider={_spider_name} - d
+        # setting = DOWNLOAD_DELAY = 2 - d
+        # arg1 = val1
+        _p = Popen(['curl', 'http://localhost:6800/schedule.json',
+                    '-d', 'project=general', '-d', f'spider={_spider_name}',
+                    '-o', f"{_path + _filename}"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        _output, _err = _p.communicate(b"stdin")
+        rc = _p.returncode
 
-        if _data['status'] == 'ok':
-            # call back when done request post to https://vendacartapi.azurewebsites.net/api/productlistimport
-            # return dumped json when done to file C:\vendart\products\new
-            _filename = utility.get_output_file(_file_name=f"\\{_spider_name}_product")
-            utility.writeJSON(os.getcwd()+_filename, _data['items'])
-            return jsonify(_data)
-        else:
-            return jsonify('something went wrong')
+        # return status code from _output
 
+
+@app.route('/update/<project_name>', methods=["GET"])
+def add_version(project_name):
+    pass
+
+
+@app.route('/projects', methods=["GET"])
+def get_projects():
+    pass
 
 @app.route('/spider/<customer_id>/<spider_name>/<username>/<password>', methods=['GET'])
 def run_prices(customer_id, spider_name, username, password):
@@ -105,32 +100,23 @@ def run_prices(customer_id, spider_name, username, password):
     # get the list of spiders
     _tmp_spider_list = get_scrapper()
     if _spider_name not in _tmp_spider_list:
-        return jsonify({"Error": f"No Spider Named {_spider_name}"})
+        abort(404)
     else:
-        # get settings file
-        # settings_file = utility.get_working_dir() + "/general/settings.json"  # the path of the settings file
-        # settings_data = utility.readJSON(settings_file)  # read settings
-        # _selected_scrapper = settings_data[_spider_name]  # read the exact scrapper
-        params = {
-            "_username": _username,
-            "_password": _password,
-            "_signin": True,
-            '_spider': _spider_name,
-            'spider_name': _spider_name,
-        }
-        # async run or pool here
-        response = requests.get('http://localhost:9080/crawl.json', params)
-        _data = json.loads(response.text)
-        # call back when done request post to https://vendacartapi.azurewebsites.net/api/pricing
-        # return dumped json when done to file C:\vendart\pricing\new
 
-        if _data['status'] == 'ok':
-            _filename = utility.get_output_file(_file_name=f"\\{_spider_name}_product")
-            utility.writeJSON(os.getcwd() + _filename, _data['items'])
-            return jsonify(_data['items'])
-        else:
-            return jsonify('something went wrong')
+#          # $ curl
+#         # http://localhost:6800/schedule.json -d project= <projectname> -d spider={_spider_name} - d
+#         # setting = DOWNLOAD_DELAY = 2 - d
+#         # arg1 = val1
+        _p = Popen(['curl', 'http://localhost:6800/schedule.json',
+                    '-d', 'project=default', '-d', f'spider={_spider_name}'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        _output, _err = _p.communicate(b"stdin")
+        rc = _p.returncode
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=80)
+
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=80)
